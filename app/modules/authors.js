8944b703949dc18e66424e41eecde92b284d5b5a
Oleg <ru-xander@yandex.ru>
2013-07-21 02:13:53 +0900

    Authors.js errors fix

--- app/modules/authors.js
+++ app/modules/authors.js
@@ -5,7 +5,7 @@ module.exports = function( db ) {
   return {
     list: function( req, res ){
       db.collection('books').find().toArray(function(err, items) {
-		if( !err ) {
+		if( items ) {
 			res.send(200, items);
 		} else {
 			res.send(204, {'description':'Empty'});
@@ -15,7 +15,7 @@ module.exports = function( db ) {
 
     view: function( req, res ){
       db.collection('books').find({'author' : req.params['id']}).toArray(function(err, items) {
-        if( !err ) {
+        if( items ) {
 			res.send(200, items);
 		} else {
 			res.send(204, {'description':'Author not found'});
