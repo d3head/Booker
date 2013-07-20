7adf20c376e926261efdaf128943958f743585d8
Oleg <ru-xander@yandex.ru>
2013-07-21 01:46:32 +0900

    Authors.js fix

--- app/modules/authors.js
+++ app/modules/authors.js
@@ -5,13 +5,13 @@ module.exports = function( db ) {
   return {
     list: function( req, res ){
       db.collection('books').find().toArray(function(err, items) {
-		res.send(200, items);
+		res.send(200, 'List: <br />' + items);
 	  });
     },
 
     view: function( req, res ){
       db.collection('books').find({'author' : req.params['id']}).toArray(function(err, items) {
-        res.send(200, items);
+        res.send(200, 'View: <br />' + items);
       });
     }, 
   }
