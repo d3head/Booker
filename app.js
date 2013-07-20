bf75dc485956251ee0e83ee64deb7b166fb6b56b
Eugene Rodionov <git@theaqua.name>
2013-07-20 19:52:28 +0400

    new connect to mongodb

--- app.js
+++ app.js
@@ -3,13 +3,15 @@
  */
 
 var express = require('express')
-  , MongoClient = require('mongodb').MongoClient
+  , mongoDB = require('mongodb').Db
+  , mongoServer = require('mongodb').Server
   , config = require('./app/config.json')
   , books = require('./app/modules/books')
   , categories = require('./app/modules/categories')
   , authors = require('./app/modules/authors');
 
-var app = express();
+var app = express(),
+    db = new mongoDB( config.db.db, new mongoServer( config.db.server, config.db.port ) );
 
 // all environments
 app.set('port', process.env.PORT || config.server.port);
@@ -30,24 +32,24 @@ app.listen(app.get('port'), function() {
 	console.log('Express server listening on port ' + app.get('port'));
 });
 
-MongoClient.connect( 'mongodb://' + config.db.server + ':' + config.db.port + '/' + config.db.db, function( err, db ) {
-	if( !err ) {
-		console.log( 'Connected to ' + config.db.server + ', using ' + config.db.db )
-	} else {
-		console.dir( err );
-	}
-  
-	app.get('/', books.list);
-
-	app.get('/books', books.list);
-	app.post('/books', books.create);
-	app.get('/books/:id', books.view);
-	app.put('/books/:id', books.edit);
-	app.del('/books/:id', books.delete);
-
-	app.get('/categories', categories.list);
-	app.get('/categories/:name', categories.view);
-
-	app.get('/authors', authors.list);
-	app.get('/authors/:id', authors.view);
+db.open( function( err, result ) {
+  if( !err ) {
+    console.log( 'Connected to ' + config.db.server + ', using database ' + config.db.db )
+  } else {
+    console.dir( err );
+  }
 } );
+
+app.get('/', books.list);
+
+app.get('/books', books.list);
+app.post('/books', books.create);
+app.get('/books/:id', books.view);
+app.put('/books/:id', books.edit);
+app.del('/books/:id', books.delete);
+
+app.get('/categories', categories.list);
+app.get('/categories/:name', categories.view);
+
+app.get('/authors', authors.list);
+app.get('/authors/:id', authors.view);
