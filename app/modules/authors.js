69980496d8cb2489003be72d99b6469af27fe92f
Eugene Rodionov <git@theaqua.name>
2013-07-20 21:35:37 +0400

    If result empty 404

--- app/modules/authors.js
+++ app/modules/authors.js
@@ -5,10 +5,10 @@ module.exports = function( db ) {
   return {
     view: function( req, res ) {
       db.collection( 'books' ).find( { 'author' : req.params[ 'id' ] } ).toArray( function( err, items ) {
-        if( items ) {
+        if( typeof items[ 0 ] !== 'undefined' && items[ 0 ] !== null ) {
     			res.send( 200, items );
     		} else {
-    			res.send( 204, { 'code': '204', 'description': 'Author not found' } );
+    			res.send( 400, { 'code': '400', 'description': 'Author not found' } );
     		}
       });
     }, 
