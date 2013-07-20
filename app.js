fa3e44fb6647178aa52f7fd503b060db5e199b70
Eugene Rodionov <git@theaqua.name>
2013-07-20 14:00:54 +0400

    Lets go

--- app.js
+++ app.js
@@ -4,31 +4,27 @@
  */
 
 var express = require('express')
-  , routes = require('./routes')
-  , books = require('./routes/books')
-  , http = require('http')
-  , path = require('path');
+  , config = require('./app/config.json')
+  , books = require('./app/modules/books');
 
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 88);
+app.set('port', process.env.PORT || config.server.port);
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(express.cookieParser('zWFihu3235dhvg7234'));
 app.use(express.session());
-app.use(app.router);
 
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
 app.get('/books', books.list);
 app.get('/books/:id', books.view);
 
-http.createServer(app).listen(app.get('port'), function(){
+app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
 });
---
+++ app/modules/books.js
@@ -0,0 +1,10 @@
+/*
+ * GET books listing.
+ */ 
+exports.list = function(req, res){
+  res.render('books');
+};
+
+exports.view = function(req, res){
+  res.render('books');
+};
