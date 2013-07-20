706cd530c0104ced0eb8c29ad058b36b9badff3b
Eugene Rodionov <git@theaqua.name>
2013-07-20 12:57:55 +0400

    Trash

--- app.js
+++ app.js
@@ -13,17 +13,12 @@ var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 88);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(express.cookieParser('zWFihu3235dhvg7234'));
 app.use(express.session());
 app.use(app.router);
-app.use(require('stylus').middleware(__dirname + '/public'));
-app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
