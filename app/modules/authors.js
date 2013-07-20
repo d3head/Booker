c42ce58be3256becc59f05e78eb24af45a09ef5f
Oleg <ru-xander@yandex.ru>
2013-07-21 01:21:24 +0900

    Authors.js update

--- app/modules/authors.js
+++ app/modules/authors.js
@@ -2,13 +2,13 @@
  * Authors
  */
 exports.list = function(req, res){
-	res.send(200, 'Authors');
-
-	db.collection('books').find().toArray(function(err, items) {});
+	db.collection('books').find().toArray(function(err, items) {
+		res.send(200, items);
+	});
 };
 
 exports.view = function(req, res){
 	db.collection('books').find({'author' : req.params[0]}).toArray(function(err, items) {
-		res.send(200, 'Authors' + items);
+		res.send(200, items);
 	});	
 };
