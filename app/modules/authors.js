e6548f974d0a29201d455f81515e0cacf03ef4ba
Oleg <ru-xander@yandex.ru>
2013-07-21 01:26:27 +0900

    Authors.js rollback

--- app/modules/authors.js
+++ app/modules/authors.js
@@ -1,21 +1,6 @@
 /*
  * Authors
  */
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-exports.list = function(req, res){
-	db.collection('books').find().toArray(function(err, items) {
-		res.send(200, items);
-	});
-};
-
-exports.view = function(req, res){
-	db.collection('books').find({'author' : req.params[0]}).toArray(function(err, items) {
-		res.send(200, items);
-	});	
-};
-=======
 module.exports = function( db ) {
   return {
     list: function( req, res ){
@@ -24,26 +9,6 @@ module.exports = function( db ) {
       db.collection('books').find().toArray(function(err, items) {});
     },
 
-=======
-module.exports = function( db ) {
-  return {
-    list: function( req, res ){
-      res.send(200, 'Authors');
-
-      db.collection('books').find().toArray(function(err, items) {});
-    },
-
->>>>>>> c501bb577cb78cb56b3e8d22dc1c5903d81292b4
-=======
-module.exports = function( db ) {
-  return {
-    list: function( req, res ){
-      res.send(200, 'Authors');
-
-      db.collection('books').find().toArray(function(err, items) {});
-    },
-
->>>>>>> c501bb577cb78cb56b3e8d22dc1c5903d81292b4
     view: function( req, res ){
       db.collection('books').find({'author' : req.params[0]}).toArray(function(err, items) {
         res.send(200, 'Authors' + items);
@@ -51,10 +16,3 @@ module.exports = function( db ) {
     }, 
   }
 }
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> c501bb577cb78cb56b3e8d22dc1c5903d81292b4
-=======
->>>>>>> c501bb577cb78cb56b3e8d22dc1c5903d81292b4
-=======
->>>>>>> c501bb577cb78cb56b3e8d22dc1c5903d81292b4
