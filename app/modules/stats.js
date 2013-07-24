9693478bc68bc6b485e315b13194cfbe68ba024f
Oleg <ru-xander@yandex.ru>
2013-07-24 22:22:02 +0900

    API for /stats/: books and added today done.

--- app/modules/stats.js
+++ app/modules/stats.js
@@ -3,28 +3,21 @@
  */
 var utils = require( './utils' );;
 // utils.unique()
-
+ 
 module.exports = function( db ) {
   return {
     get: function( req, res ) {
-		
-			var result = [ ],
-          today = new Date(),
-          addedToday,
-          authors,
-          tags,
-          books;
-
       db.collection( 'books' ).count( function( err, count ) {
-        result['books'] = count;
+        var result = { },
+            today = new Date();
+            
+        result.books = count;
+        
+        db.collection( 'books' ).find( { 'added': today } ).count( function( err, count ) {
+                result.addedToday = count;
+                res.send( 200, result );
+        } );
       } );
-      
-      db.collection( 'books' ).find( { 'added': today} ).count( function( err, count ) {
-        result['addedToday'] = count;
-      } );
-
-      console.log( result );
-      res.send( 200, result );
-    }, 
+    }
   }
 }
