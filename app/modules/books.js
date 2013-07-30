cb57beb0ff459116cef93b58ca708127034ee4eb
Oleg <ru-xander@yandex.ru>
2013-07-31 05:38:59 +0900

    API for /books/: added tags support

--- app/modules/books.js
+++ app/modules/books.js
@@ -16,14 +16,32 @@ module.exports = function( db ) {
     create: function( req, res ) {
       var request = req.body;
 
-      if( request.title && request.description && request.author && request.category ) {
+      if( request.title && request.description && request.author && request.category && request.tags ) {
         db.collection( 'books' ).save( {
           _id: request.title,
           title: request.title,
           description: request.description,
           author: request.author,
-          category: request.category
+          category: request.category,
+					tags: request.tags
         } );
+				
+				for( var i = 0; i < request.tags.length; i++ ) {
+					db.collection( 'tags' ).find( title : request.tags[i] ).toArray( function( err, items ) {
+						if( items.length > 0 ) {
+							db.collection( 'tags' ).update( { 
+								title: request.tags[i]
+							}, {
+								$inc: { books : 1 }
+							} );
+						} else {
+							db.collection( 'tags' ).save( {
+								title: request.tags[i]
+								books: 1
+							} );
+						}
+					}						
+				}
 
         res.send( 201, { 'status': 'ok', 'code': '201', 'description': 'Book ' + request.title + ' now available on /books/' + request.title } );
 
@@ -35,6 +53,8 @@ module.exports = function( db ) {
         res.send( 400, { 'status': 'error', 'code': '400', 'description': 'Missed author' } );
       } else if( !request.category ) {
         res.send( 400, { 'status': 'error', 'code': '400', 'description': 'Missed category' } );
+      } else if( !request.tags ) {
+        res.send( 400, { 'status': 'error', 'code': '400', 'description': 'Missed tags' } );
       } 
     },
 
@@ -53,7 +73,7 @@ module.exports = function( db ) {
     edit: function( req, res ) {
       var request = req.body;
 
-      if( request.old && request.title && request.description && request.author && request.category ) {
+      if( request.old && request.title && request.description && request.author && request.category && request.tags ) {
         db.collection( 'books' ).update( { 
           title: request.old
         }, {
@@ -61,8 +81,26 @@ module.exports = function( db ) {
           title: request.title,
           description: request.description,
           author: request.author,
-          category: request.category
+          category: request.category,
+					tags: request.tags
         } );
+				
+				for( var i = 0; i < request.tags.length; i++ ) {
+					db.collection( 'tags' ).find( title : request.tags[i] ).toArray( function( err, items ) {
+						if( items.length > 0 ) {
+							db.collection( 'tags' ).update( { 
+								title: request.tags[i]
+							}, {
+								$inc: { books : 1 }
+							} );
+						} else {
+							db.collection( 'tags' ).save( {
+								title: request.tags[i]
+								books: 1
+							} );
+						}
+					}						
+				}
 
         res.send( 201, { 'status': 'ok', 'code': '200', 'description': 'Updated book ' + request.title + ' now available on /books/' + request.title } );
       } else if( !request.old ) {
@@ -75,6 +113,8 @@ module.exports = function( db ) {
         res.send( 400, { 'status': 'error', 'code': '400', 'description': 'Missed author' } );
       } else if( !request.category ) {
         res.send( 400, { 'status': 'error', 'code': '400', 'description': 'Missed category' } );
+      } else if( !request.tags ) {
+        res.send( 400, { 'status': 'error', 'code': '400', 'description': 'Missed tags' } );
       } 
     },
 
@@ -83,8 +123,22 @@ module.exports = function( db ) {
 
       if( title ) {
         db.collection( 'books' ).remove( { 
-          title: title
+          _id: title
         } );
+				
+			db.collection( 'books' ).find( { '_id': title } ).distinct( 'tags', {}, function( err, tags ) {
+				for( var i = 0; i < tags.length; i++ ) {
+					db.collection( 'tags' ).find( title : tags[i] ).toArray( function( err, items ) {
+						if( items.length > 0 ) {
+							db.collection( 'tags' ).update( { 
+								title: tags[i]
+							}, {
+								$inc: { books : -1 }
+							} );
+						}
+					}	);
+				}
+			}
 
         res.send( 201, { 'status': 'ok', 'code': '200', 'description': 'Book ' + title + ' removed' } );
       } else if( !title ) {
